62a4646a64e0867bb6abe28abad6b7a9b46a2365
Lisseth00 <lissethrivas1328@gmail.com>
2025-11-01 11:07:51 -0500

    Se mejoro la barra de busquedaad junto a las vista de los sub-servivios

--- app/Http/Controllers/AjustesController.php
+++ app/Http/Controllers/AjustesController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Servicios;
 use App\Models\Cotizacion;
+use App\Models\SubServicios;
 use Barryvdh\DomPDF\Facade\Pdf;
 
 class AjustesController extends Controller
@@ -11,13 +12,14 @@ class AjustesController extends Controller
     public function index()
     {
         $servicios = Servicios::all();
+        $subServicios = SubServicios::with('servicio')->orderBy('id', 'asc')->get();
         
         // Cargar cotizaciones con relaciones para el historial
         $cotizaciones = Cotizacion::with(['persona', 'subServicio.servicio'])
             ->orderBy('fecha_cotizacion', 'desc')
             ->get();
         
-        return view('usuarios.ajustes', compact('servicios', 'cotizaciones'));
+        return view('usuarios.ajustes', compact('servicios', 'subServicios', 'cotizaciones'));
     }
 
     public function exportHistorialPdf()
